dfb6a9c41dfbed757995ae849929f3bdfa8a94f1
tide1218 <s91424@infoseek.jp>
2017-09-08 20:27:25 +0900

    ADD wall_stop.launch

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -2,7 +2,7 @@
 
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
-cd ~/catkin_ws
+cd ~/catkin_ws/src
 #catkin_make -j8 || catkin_make -j8 #CPU2個だけど8並列頑張ってね。しかも1回目コケたらもう一回やってね
 
 git clone --depth=1 https://github.com/citueda/pimouse_ros.git
